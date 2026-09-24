4c5d0824005d0054abfa05afabff726cd085c83f
melnaeema-eng <melnaeema@gmail.com>
2026-09-24 15:13:37 +0300

    CMMS: professional guided project setup

--- src/generated/buildInfo.js
+++ src/generated/buildInfo.js
@@ -1,1 +1,1 @@
-export const BUILD_INFO={"commit":"8bc6093","branch":"main","builtAt":"2026-09-24T11:47:22.054Z"}
+export const BUILD_INFO={"commit":"7b747c3","branch":"main","builtAt":"2026-09-24T12:13:35.127Z"}
